268714def585b1c56fae0e08c27c0de6b3dab107
MistaKronos <sindresteen007@hotmail.co.uk>
2023-11-06 14:22:05 +0100

    picture upload and usage added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+var path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page + load 3 pictures */
+router.get("/", function (req, res, next) {
+  const pictures = fs
+    .readdirSync(path.join(__dirname, "../pictures/"))
+    .slice(0, 3);
+  res.render("index", { pictures: pictures, title: "Express" });
 });
 
 module.exports = router;
